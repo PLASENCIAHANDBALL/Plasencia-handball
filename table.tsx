605d1c79b4efeae6f59fd406324f26c67a993b71
PLASENCIAHANDBALL <christianlinarescarsi@gmail.com>
2025-11-12 23:22:55 +0100

    Delete table.tsx

--- table.tsx
+++
@@ -1,2 +0,0 @@
-import * as React from 'react'
-export function Table(p:any){return <table {...p}/>}export function TableHeader(p:any){return <thead {...p}/>}export function TableBody(p:any){return <tbody {...p}/>}export function TableRow(p:any){return <tr {...p}/>}export function TableHead(p:any){return <th {...p}/>}export function TableCell(p:any){return <td {...p}/>}
