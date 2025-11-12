ed543cdeeec46a263e7d238e76d3e9344eac3fae
PLASENCIAHANDBALL <christianlinarescarsi@gmail.com>
2025-11-12 01:16:33 +0100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: ["class"],
-  content: ["./index.html","./src/**/*.{ts,tsx}"],
-  theme: { extend: {} },
-  plugins: []
-}
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
