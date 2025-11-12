6df5f1bd5552e5c63c44fd41a095a456cc84105a
PLASENCIAHANDBALL <christianlinarescarsi@gmail.com>
2025-11-12 23:19:29 +0100

    Delete card.tsx

--- card.tsx
+++
@@ -1,2 +0,0 @@
-import * as React from 'react'
-export function Card({className='',...p}:any){return <div className={className} {...p}/>}export function CardHeader(p:any){return <div {...p}/>}export function CardContent(p:any){return <div {...p}/>}export function CardTitle(p:any){return <h3 {...p}/>}
