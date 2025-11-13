d03229dbb1de8abeca2678c42978224546ee50d1
PLASENCIAHANDBALL <christianlinarescarsi@gmail.com>
2025-11-13 03:39:31 +0100

    Delete Plasencia-handball directory

--- Plasencia-handball/src/components/ui/dialog.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react'
-const C=React.createContext<any>(null)
-export function Dialog({open,onOpenChange,children}:any){const [o,setO]=React.useState(!!open);React.useEffect(()=>{if(open!==undefined) setO(open)},[open]);return <C.Provider value={{open:o,setOpen:(b:boolean)=>{onOpenChange?onOpenChange(b):setO(b)}}}>{children}</C.Provider>}
-export function DialogTrigger({asChild,children}:any){return children}
-export function DialogContent(p:any){return <div {...p}/>}
-export function DialogHeader(p:any){return <div {...p}/>}
-export function DialogTitle(p:any){return <h2 {...p}/>}
