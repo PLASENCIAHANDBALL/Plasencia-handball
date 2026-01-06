544fd39836f848e30255884d0c9dc27072ae6958
PLASENCIAHANDBALL <christianlinarescarsi@gmail.com>
2026-01-06 22:41:19 +0100

    Delete src directory

--- src/components/BackgroundArt.tsx
+++
@@ -1,2 +0,0 @@
-import React from 'react'
-export default function BackgroundArt(){ return (<div aria-hidden className='pointer-events-none fixed inset-0 -z-10'><div className='absolute inset-0 bg-gradient-to-br from-sky-50 to-white'/></div>) }
